8d7cfad25c921eb227e8e3a9c6b17c78709b84dd
Dávid Jozef Hreško <david.jozef.hresko@student.tuke.sk>
2018-11-16 18:43:03 +0100

    level loading added

---
+++ README.md
@@ -0,0 +1,6 @@
+# FlowFree
+
+Bot for playing Flow Free game, using backtracking.
+
+Made with love by Kubo and David
+
--- game.py
+++ game.py
@@ -1,5 +1,4 @@
 import pygame
-import thorpy
 import json
 
 # game constants
@@ -38,10 +37,12 @@ def parse_color_from_json(color):
 
 
 # load levels data from json file
-def parse_levels_file(loaded_level):
+def load_level(loaded_level):
     with open('assets/levels.json') as levels_file:
         levels_file = json.load(levels_file)
         level_one = levels_file["levels"][loaded_level]
+
+        print(loaded_level)
 
         for dot in level_one["dots"]:
             pygame.draw.circle(grid_surface, parse_color_from_json(dot["color"]), (dot["x"], dot["y"]), 25)
@@ -51,64 +52,82 @@ def parse_levels_file(loaded_level):
 def generate_new_level():
     global current_level
 
-    parse_levels_file(current_level)
-
-    if current_level < 3:
+    if current_level < 2:
         current_level += 1
     else:
-        current_level = 1
+        current_level = 0
+
+    load_level(current_level)
 
 
-# game init
-pygame.init()
-clock = pygame.time.Clock()
-icon = pygame.image.load('assets\logo.jpg')
-pygame.display.set_icon(icon)
-pygame.display.set_caption("Flow free")
+# handles click events on button
+def handle_click_buttons():
+    mouse_position = pygame.mouse.get_pos()
 
-# surfaces
-main_surface = pygame.display.set_mode((WIDTH, HEIGHT))
-main_surface.fill(GREY)
+    # restart button click handling
+    if 550 > mouse_position[0] > 400 and 320 > mouse_position[1] > 290:
+        load_level(current_level)
 
-grid_surface = pygame.Surface((360, 360))
-grid_surface.fill(GREY)
+    # new level button click handling
+    elif 550 > mouse_position[0] > 400 and 370 > mouse_position[1] > 340:
+        generate_new_level()
 
-# fonts
-text_font = pygame.font.SysFont("comicsansms", 24)
-text_flows_label = text_font.render("Flows", False, WHITE)
-text_flows_value = text_font.render("0/6", False, PURPLE)
-text_pipes_label = text_font.render("Pipe", False, WHITE)
-text_pipes_value = text_font.render("0 %", False, PURPLE)
-text_moves_label = text_font.render("Moves", False, WHITE)
-text_moves_value = text_font.render("0", False, PURPLE)
 
-# buttons
-button_restart_level = thorpy.make_button("Restart level")
-button_restart_level.set_topleft((400, 290))
-button_restart_level.set_font("comicsansms")
-button_restart_level.set_size((150, 30))
+# init game
+def init_game():
+    global clock
 
-button_new_level = thorpy.make_button("New level", func=generate_new_level)
-button_new_level.set_topleft((400, 340))
-button_new_level.set_font("comicsansms")
-button_new_level.set_size((150, 30))
+    pygame.init()
+    clock = pygame.time.Clock()
+    icon = pygame.image.load('assets\logo.jpg')
+    pygame.display.set_icon(icon)
+    pygame.display.set_caption("Flow free")
 
-for x in range(0, 6):
-    for y in range(0, 6):
-        pygame.draw.rect(grid_surface, GREEN_CYAN, [60 * y, 60 * x, 60, 60], 1)
 
-# buttons rendering
-button_restart_level.blit()
-button_new_level.blit()
+# generate surfaces
+def generate_surfaces():
+    global main_surface, grid_surface
 
-# text rendering
-main_surface.blit(grid_surface, (10, 10))
-main_surface.blit(text_flows_label, (400, 30))
-main_surface.blit(text_flows_value, (490, 30))
-main_surface.blit(text_pipes_label, (400, 70))
-main_surface.blit(text_pipes_value, (490, 70))
-main_surface.blit(text_moves_label, (400, 110))
-main_surface.blit(text_moves_value, (490, 110))
+    main_surface = pygame.display.set_mode((WIDTH, HEIGHT))
+    main_surface.fill(GREY)
+    grid_surface = pygame.Surface((360, 360))
+    grid_surface.fill(GREY)
+
+    for x in range(0, 6):
+        for y in range(0, 6):
+            pygame.draw.rect(grid_surface, GREEN_CYAN, [60 * y, 60 * x, 60, 60], 1)
+
+
+# generate fonts
+def generate_fonts():
+    text_font = pygame.font.SysFont("comicsansms", 24)
+    text_flows_label = text_font.render("Flows", False, WHITE)
+    text_flows_value = text_font.render("0/6", False, PURPLE)
+    text_pipes_label = text_font.render("Pipe", False, WHITE)
+    text_pipes_value = text_font.render("0 %", False, PURPLE)
+    text_moves_label = text_font.render("Moves", False, WHITE)
+    text_moves_value = text_font.render("0", False, PURPLE)
+
+    main_surface.blit(grid_surface, (10, 10))
+    main_surface.blit(text_flows_label, (400, 30))
+    main_surface.blit(text_flows_value, (490, 30))
+    main_surface.blit(text_pipes_label, (400, 70))
+    main_surface.blit(text_pipes_value, (490, 70))
+    main_surface.blit(text_moves_label, (400, 110))
+    main_surface.blit(text_moves_value, (490, 110))
+
+
+# generate buttons
+def generate_buttons():
+    pygame.draw.rect(main_surface, WHITE, (400, 290, 150, 30))
+    pygame.draw.rect(main_surface, WHITE, (400, 340, 150, 30))
+
+
+init_game()
+generate_surfaces()
+load_level(current_level)
+generate_fonts()
+generate_buttons()
 
 while running:
     clock.tick(FPS)
@@ -116,5 +135,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            handle_click_buttons()
 
     pygame.display.flip()
